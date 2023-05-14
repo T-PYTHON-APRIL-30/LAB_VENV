2994f5f857eb0addd6241bf72694dda9b9b8bd20
NouraAldayhani <nouraa.sultan9@gmail.com>
2023-05-14 14:59:26 +0300

    Solved the lab

---
+++ text_art.py
@@ -0,0 +1,8 @@
+from art import *
+
+Art= text2art("BELIEVE and ACHEIVE",font='block-medium',chr_ignore=True )
+print(Art)
+
+print(text2art("N O U R A","white_bubble")) 
+print("\n")
+print(text2art("HELLO","rnd-medium")) 
