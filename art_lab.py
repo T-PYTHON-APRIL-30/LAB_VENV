e035e886b23c6905d10e5cc7e53bb7dafa336882
juhaina <jh75n.awad@gmail.com>
2023-05-14 15:11:17 +0300

    Lab Art

---
+++ art_lab.py
@@ -0,0 +1,9 @@
+from art import *
+
+Art=text2art("BELIEVE and ACHEIVE",font='block',chr_ignore=True)
+print(Art)
+
+#Bouns Come up with different phrases with different art and decoration:
+Art1=text2art("you are a mazing","random")
+print(Art1)
+tprint("Juhaina","rand") 
