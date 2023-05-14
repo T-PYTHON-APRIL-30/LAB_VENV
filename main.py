dd840d5242e2c91e067a60a172f2565145af7b16
khalid <10abha10@gmail.com>
2023-05-14 14:56:37 +0300

    solved

---
+++ main.py
@@ -0,0 +1,15 @@
+from art import text2art,tprint,randart,aprint
+
+
+
+text=text2art("BELIEVE and ACHEIVE",font="block")
+print(text)
+print(text2art("khalid",font="small"))
+
+
+tprint("hello",font="cybermedium")
+tprint("we l c o m e","rnd-xlarge")
+print(randart())
+aprint("Sword")
+aprint("cofee")
+aprint("Needle",number=2)
