0be80511563aa34c88adba935b67d1e060ceff8e
Dalia <daliaasq@gmail.com>
2023-05-14 14:59:38 +0300

    Venv Lab + Bonus

---
+++ main.py
@@ -0,0 +1,19 @@
+from art import *
+
+
+#normal
+print("BELIEVE and ACHEIVE")
+#using tprint from art package
+tprint("BELIEVE and ACHEIVE")
+#using tprint from art package with block font
+tprint("BELIEVE and ACHEIVE", font="block")
+
+#Bonus
+
+#normal
+print("POWER")
+#using tprint from art package with different font
+tprint("POWER", font="weird")
+tprint("POWER", font="black2")
+tprint("POWER", font="rnd-xlarge")
+tprint("POWER",font="rnd-large")
