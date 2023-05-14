9c116ce4e175ed9833ec67d19b51238af1a84b6b
Bakr Hawsawi <122125253+Bakr40@users.noreply.github.com>
2023-05-14 14:54:47 +0300

    finsh

---
+++ main.py
@@ -0,0 +1,14 @@
+from art import *
+tprint("BELIEVE and ACHEIVE", font ="block")
+
+tprint("Well done is better than well said", font= "random")
+
+tprint("Once you choose hope, anything's possible", font="bulbhead")
+
+tprint("Try it again. Fail again. Fail better","white_bubble")
+
+tprint("You only live once but if you do it right, once is enough",font= "random")
+
+aprint("butterfly")
+aprint("happy")
+
