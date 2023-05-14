95c14b64c8891ca2c01a89b4aa7b3fb1a5ec459b
Abdulaziz <Abdulaziz-A1997@hotmail.com>
2023-05-14 19:57:20 +0300

    art lab solved

---
+++ art_lab.py
@@ -0,0 +1,11 @@
+from art import *
+
+#1_________________________________________________________________________________
+ert = text2art("BELIEVE and ACHEIVE",font='block')
+
+print(ert)
+
+#bonus_____________________________________________________________________________
+rt = text2art("nothing ventured nothing gained",font='small',decoration="wave3")
+
+print(rt)
