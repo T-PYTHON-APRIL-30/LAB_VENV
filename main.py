6e2361a2cc3ad43208c9ba26a3acef289c2c457a
1947860@uj.edu.sa <1947860@uj.edu.sa>
2023-05-14 14:55:41 +0300

    solved+bonus

---
+++ main.py
@@ -0,0 +1,15 @@
+from art import*
+
+Art=text2art("BELIEVE and ACHEIVE",font='block',chr_ignore=True)
+
+print(Art)
+
+#bonus
+
+tprint("Yes","white_bubble")
+
+tprint("angary",font="rnd-small")
+
+tprint("test",font="rnd-large")
+
+tprint("1","wizard")
