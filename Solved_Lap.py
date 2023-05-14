0b6e3e69ea7a7c5acd6563fa52275561dd1d6048
Rayan Hantoul <ryan-055@outlook.com>
2023-05-14 20:59:24 +0300

    Solved

---
+++ Solved_Lap.py
@@ -0,0 +1,13 @@
+from art import *
+
+art1 = text2art("BELIEVE",'block')
+print(art1)
+
+art2 =text2art("ACHEIVE","block")
+print(art2)
+
+
+#Bonus
+
+phrase1 = text2art("WORLD", "upset")
+print(phrase1)
