349046a81e517fe8e59ace36fe1f7677aba918b5
saud <sasud000@gmail.com>
2023-05-15 11:05:35 +0300

    bouns

---
+++ bouns.py
@@ -0,0 +1,16 @@
+from art import *
+
+def print_text_art(text, font):
+    text_art = text2art(text, font)
+    print(text_art)
+
+print_text_art("BELIEVE and ACHIEVE", "block")
+
+# Bonus: Additional phrases with different art and decoration
+print_text_art("Never Stop Learning", "lean")
+print_text_art("Keep Pushing", "marquee")
+print_text_art("The Sky Is The Limit", "roman")
+print(decor("barcode1"))  # Line of decoration
+print_text_art("Stay Positive", "cybermedium")
+print_text_art("You Can Do It", "banner3-D")
+print(decor("barcode1"))  # Line of decoration
